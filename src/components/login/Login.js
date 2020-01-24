1c665fc29985308efe4594395eeb73b6e5f142df
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2020-01-23 23:18:28 -0500

    I started with basic styles to login with grid layout

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,16 +2,20 @@ import React from "react";
 
 export default function Login() {
   return (
-    <section className="login">
-      <section className="login__title">Login</section>
-      <section className="login__user">
-        User :<input type="text"></input>
-      </section>
-      <section className="login__pass">
-        Password :<input type="Password"></input>
-      </section>
-      <section className="login__buttons">
-        <button>Send</button>
+    <section className="wrapper__login">
+      <section className="login">
+        <section className="login__title">Login</section>
+        <section className="login__user">
+          <label>User :</label>
+          <input type="text" />
+        </section>
+        <section className="login__pass">
+          <label>Password</label>
+          <input type="Password" />
+        </section>
+        <section className="login__buttons">
+          <button>Send</button>
+        </section>
       </section>
     </section>
   );
